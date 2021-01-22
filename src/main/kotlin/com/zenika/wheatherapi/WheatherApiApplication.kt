9617f69870757d05427163f51ab71413c709f1e9
Christophe TARET <ch.taret@gmail.com>
2021-01-22 21:40:37 +0100

    chore: init spring project

---
+++ src/main/kotlin/com/zenika/wheatherapi/WheatherApiApplication.kt
@@ -0,0 +1,11 @@
+package com.zenika.wheatherapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WheatherApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<WheatherApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/zenika/wheatherapi/WheatherApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zenika.wheatherapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WheatherApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
